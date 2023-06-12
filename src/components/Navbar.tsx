5ba7605b5de1b2bdece8d7fbfef459ea21eb8fbf
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2023-06-12 17:39:43 +0600

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import {Link} from 'react-router-dom';
 import {BsFillCartFill} from 'react-icons/bs';
+import {useContext} from 'react';
+import { ShoppingCartContext } from '../context/ShoppingCartContext';
 const Navbar = () => {
+  const {cartAmount} = useContext(ShoppingCartContext)
   return (
     <nav className="lg mb-3 container mx-auto bg-white shadow-xl p-4 sticky top-0">
         <div className="flex items-center justify-center space-x-16 text-xl uppercase">
@@ -10,7 +13,7 @@ const Navbar = () => {
            <div className="cursor-pointer flex relative group">
             <BsFillCartFill className="text-3xl group-hover:text-red-500"></BsFillCartFill>
                 <div className="absolute shadow-xl bg-red-500 h-[20px] w-[20px] -right-2 -bottom-2 rounded-full flex justify-center items-center text-[12px] text-white group-hover:text-red-500 group-hover:bg-black font-bold">
-                    0
+                    {cartAmount}
                 </div>
            </div>
         </div>
