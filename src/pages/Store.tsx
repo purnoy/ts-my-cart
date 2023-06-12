2c57d63d56ee8c94366214479e8e528767c86988
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2023-06-12 17:11:19 +0600

    Update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -2,6 +2,16 @@ import { useEffect, useState } from "react";
 import StoredItem from "../components/StoredItem";
 
 
+
+type StoredItemProps = {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+} 
+
 const Store = () => {
   const [products, setProducts] = useState([]);
   useEffect(()=>{
@@ -13,13 +23,13 @@ const Store = () => {
     productList();
   },[]);
   
-  const catProd = products.filter(product =>{
+  const catProd = products.filter((product:StoredItemProps) =>{
     return product.category === "men's clothing" || product.category === "women's clothing";
   })
   return (
     <div className="container mx-auto max-w-[1260px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 py-10">
       {
-        catProd.map(product=>{
+        catProd.map((product:StoredItemProps)=>{
           
           return (
               <StoredItem key={product.id} product={product} />
