d9c6cd2ef124a497d7d435d18ee935b7ba250276
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2023-06-13 14:20:36 +0600

    Update ShoppingCart.tsx

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,52 +1,42 @@
-
 import {useContext} from 'react';
-import { ShoppingCartContext } from '../context/ShoppingCartContext';
+import { ShoppingCartContext } from "../context/ShoppingCartContext";
 
+
+type cartPropsType = {
+    isCartOpen: boolean;
+  }
+
+  type CartItemType = {
+    id: number;
+    amount: number;
+  }
   
-
-
-
-  
-const StoredItem = ({product}:any) => {
-
-    const {id, title, price,  image} = product;
-
-     
-    const {getAmount, increaseAmount, decreaseAmount, deleteAmount } = useContext(ShoppingCartContext);
-
-    //Amount Adding to the product
-    const amount = getAmount(product);
-
+const ShoppingCart = ({isCartOpen}:cartPropsType) => {
+    const {closeCart, cartAmount, cartItems } = useContext(ShoppingCartContext);
+    
   return (
-    <div className="rounded-md flex flex-col p-2   border">
-        <div className="cursor-pointer hover:p-4 h-[300px] p-8 duration-500">
-            <img src={image} alt="" className="h-[100%] object-cover " />
-        </div>
-        <div className="">
-            <span className=" text-[14px] cursor-pointer hover:underline">{title}</span>
-        </div>
-        <div className="my-3">
-            <span className="font-bold">Price - ${price}</span>
-        </div>
-        <div className="w-[100%] flex justify-center">
-            {
-                //Adding product to the cart
-               amount === 0?(<button onClick={()=>increaseAmount(product)} className=" block bg-red-500 w-[100%] text-white py-2 rounded-md shadow-md">Add to cart</button>): 
-               (<div className=" flex items-center justify-around w-[100%]">
-                    <div className="flex justify-around w-[50%]">
-                        {/* amount decrement */}
-                        <button onClick={()=>decreaseAmount(product)}  className=" w-[30px] text-white rounded-md bg-red-500 flex items-center justify-center">-</button>
-                        <span>{amount}</span>
-                        {/* amount decrement */}
-                        <button onClick={()=>increaseAmount(product)} className="  w-[30px]  text-white rounded-md flex items-center bg-red-500 justify-center">+</button>
-                    </div>
-                    {/* delete amount*/}
-                    <div onClick={()=>deleteAmount(id)} className="text-white rounded-md flex items-center bg-red-500 justify-center px-3 cursor-pointer">Remove</div>
-               </div>)
-            }
+    <div>
+        <div className={`${isCartOpen?'right-[0]' : '-right-[100%]'} fixed h-[100vh] w-[100%] md:w-[45%] bg-slate-50  shadow-xl p-5 top-0 z-20 duration-500 `}>
+            <div className="flex p-5 justify-between items-center">
+                <p>Shopping Cart({cartAmount})</p>
+                <p className="3xl font-bold cursor-pointer" onClick={closeCart}>X</p>
+            </div>
+            <div className="mt-5 px-5 flex flex-col w-[100%]">
+                {
+                    cartItems.map((item:CartItemType) =>{
+                        return(
+                            <div key={item.id} className="flex items-center justify-between max-w-[100%]">
+                                <p>{item.id}</p>
+                                <p>b</p>
+                            </div>
+                        )
+                    })
+                }
+                
+            </div>
         </div>
     </div>
   )
 }
 
-export default StoredItem
+export default ShoppingCart
