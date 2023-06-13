822796d761a680f13d8c7092f53f04214bd470f0
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2023-06-13 10:46:43 +0600

    Update ShoppingCart.tsx

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -6,7 +6,7 @@ type cartPropsType = {
     isCartOpen: boolean;
   }
 const ShoppingCart = ({isCartOpen}:cartPropsType) => {
-    const {closeCart, cartItems, openCart} = useContext(ShoppingCartContext);
+    const {closeCart} = useContext(ShoppingCartContext);
   return (
     <div>
         <div className={`${isCartOpen?'right-[0]' : '-right-[100%]'} absolute h-[100vh] w-[100%] md:w-[35%] bg-slate-50  shadow-xl p-5 top-0 z-100 duration-500 `}>
