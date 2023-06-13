9a92c3c29d7f24a5082fb0e59cc9c0687acfd064
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2023-06-13 14:15:06 +0600

    Update ShoppingCart.tsx

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -5,14 +5,34 @@ import { ShoppingCartContext } from "../context/ShoppingCartContext";
 type cartPropsType = {
     isCartOpen: boolean;
   }
+
+  type CartItemType = {
+    id: number;
+    amount: number;
+  }
+  
 const ShoppingCart = ({isCartOpen}:cartPropsType) => {
-    const {closeCart} = useContext(ShoppingCartContext);
+    const {closeCart, cartAmount, cartItems } = useContext(ShoppingCartContext);
+    
   return (
     <div>
-        <div className={`${isCartOpen?'right-[0]' : '-right-[100%]'} absolute h-[100vh] w-[100%] md:w-[35%] bg-slate-50  shadow-xl p-5 top-0 z-100 duration-500 `}>
+        <div className={`${isCartOpen?'right-[0]' : '-right-[100%]'} fixed h-[100vh] w-[100%] md:w-[45%] bg-slate-50  shadow-xl p-5 top-0 z-20 duration-500 `}>
             <div className="flex p-5 justify-between items-center">
-                <p>Shopping Cart</p>
-                <p onClick={closeCart}>X</p>
+                <p>Shopping Cart({cartAmount})</p>
+                <p className="3xl font-bold cursor-pointer" onClick={closeCart}>X</p>
+            </div>
+            <div className="mt-5 px-5 flex flex-col w-[100%]">
+                {
+                    cartItems.map((item:CartItemType) =>{
+                        return(
+                            <div key={item.id} className="flex items-center justify-between max-w-[100%]">
+                                <p>{item.id}</p>
+                                <p>b</p>
+                            </div>
+                        )
+                    })
+                }
+                
             </div>
         </div>
     </div>
