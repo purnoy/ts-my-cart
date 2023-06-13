8efa2d2408d92060e1e11ff9c94d7740cd51ef2a
Minhajul Hoque <90159530+purnoy@users.noreply.github.com>
2023-06-13 14:18:28 +0600

    Update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -25,12 +25,12 @@ const Store = () => {
   
 
   return (
-    <div className="container mx-auto max-w-[1260px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 py-10">
+    <div className="container mx-auto max-w-[1260px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 py-10 relative ">
       {
         products.map((product:StoredItemProps)=>{
           
           return (
-              <StoredItem key={product.id} product={product} />
+              <StoredItem key={product.id} product={product}  />
           )
         })
       }
